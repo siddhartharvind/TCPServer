2491c3532455e505cca45803ed7ff13ca7757217
Siddharth Arvind <sidarvind11@gmail.com>
2024-01-27 12:17:29 +0530

    Allow server to maintain multiple connections
    Previously, the server would listen on the port, and would accept
    the first client that requested a connection. Once the client
    sent a message and got a response back, the server would simply
    abruptly terminate. This meant that other clients attempting to
    connect would be stuck waiting, and then suddenly dropped. This
    effectively restricted the server to a single client.
    
    To fix this, we move the `accept()` call into a loop. For every
    new client attempting to connect, the function would be run to try
    and assign a new socket for it. This allows mutiple clients to
    connect to the server, which can be tested with multiple terminal
    instances running `$ telnet localhost 8888`.
    
    NOTE: The server still can only service one client at a time, with
    the rest in the wait queue. Only after the first client sends a
    message to the server and gets a response can the server accept a
    connection from the next client in the queue. This means that the
    server functions in a single-threaded/serial manner.

--- src/serial/server.c
+++ src/serial/server.c
@@ -66,47 +66,52 @@ int main(int argc, char *argv[])
     struct sockaddr_in client_addr;
     int client_len = sizeof client_addr;
 
-    int new_socketfd = accept(
+    int new_socketfd;
+    while ((new_socketfd = accept(
         socket_fd,
         (struct sockaddr *)&client_addr,
         (socklen_t *)&client_len
-    );
+    )) > 0)
+    {
+
+        printf("Connection with %d successfully established!\n", new_socketfd);
+
+
+        // 5A. Send data to client
+        char *message = "Hello connection! Enter something:\n";
+        send(new_socketfd, message, strlen(message), 0);
+
+
+        // 5B. Receive data from client
+        char client_message[255];
+
+        int read_size = recv(new_socketfd, &client_message, sizeof client_message, 0);
+
+        if (read_size == 0)
+        {
+            puts("Client disconnected.");
+            fflush(stdout);
+        }
+        else if (read_size < 0)
+        {
+            perror("ERROR: recv() failed");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            // Remove trailing newline -> '\n' or '\r\n'
+            // strcspn() -> returns first index of '\r' or '\n'
+            // By setting to '\0' => effectively remove from string
+            int msg_len = strcspn(client_message, "\r\n");
+            client_message[msg_len] = '\0';
+            dprintf(new_socketfd, "You said: \"%*s\"\n", msg_len, client_message);
+        }
+    }
 
     if (new_socketfd < 0)
     {
         perror("ERROR: Server failed to accept connection");
         exit(EXIT_FAILURE);
-    }
-
-    printf("Connection with %d successfully established!\n", new_socketfd);
-
-
-
-    // 5A. Send data to client
-    char *message = "Hello connection! Enter something:\n";
-    send(new_socketfd, message, strlen(message), 0);
-
-
-    // 5B. Receive data from client
-    char client_message[255];
-
-    int read_size = recv(new_socketfd, &client_message, sizeof client_message, 0);
-
-    if (read_size == 0)
-    {
-        puts("Client disconnected.");
-        fflush(stdout);
-    }
-    else if (read_size < 0)
-    {
-        perror("ERROR: recv() failed");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        int msg_len = strcspn(client_message, "\r\n");
-        client_message[msg_len] = '\0';
-        dprintf(new_socketfd, "You said: \"%*s\"\n", msg_len, client_message);
     }
 
 
