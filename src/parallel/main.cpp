779be553379c3998020075fc046dec6557bb42f5
Siddharth Arvind <sidarvind11@gmail.com>
2024-02-07 19:16:36 +0530

    Separate out client handling logic into function
    As part of moving the server from a singlethreaded design to a
    multithreaded one, we separate out the part of the logic that
    handles interacting with a client. We move it into its own
    function `handle_connection()`. This will help in subsequent work
    when employing threads, as each new client can be handled by a
    newly-spawned thread.
    
    In order to keep it working, we move the `KV_DATASTORE` variable
    out of `main()` and make it a global variable. Otherwise, it would
    not be accessible from within `handle_connection()` unless we pass
    a reference to it as an argument.
    
    Additionally, the `accept_connections:` label with `goto` was
    unnecessary in the original code, as it could have been expressed
    with `continue` instead. The use of `goto` in this case was not
    necessary.

--- src/parallel/main.cpp
+++ src/parallel/main.cpp
@@ -1,5 +1,5 @@
 /* 
- * A multithreaded TCP echo server 
+ * A multithreaded TCP echo server
  * Usage: tcpserver <port>
  * 
  * Testing:
@@ -19,10 +19,160 @@
 #include <unordered_map>
 
 
+
+// Global variable
+// Probably not good practice, but easier for the time being
+static std::unordered_map<std::string, std::string> KV_DATASTORE;
+
+
+void handle_connection(int client_sock)
+{
+    std::printf("Connection with %d successfully established!\n", client_sock);
+
+    // Receive data from client
+    std::string client_message { "" };
+    int read_size;
+    // size_t end_pos = 0;
+
+    static const std::string END_MESSAGE = "END\r\n";
+
+    // while (client_message.find("END", end_pos) == std::string::npos)
+    while (client_message.find("END") == std::string::npos)
+    {
+        char buffer[255];
+        read_size = recv(client_sock, &buffer, sizeof(buffer), 0);
+
+        if (read_size == 0)
+        {
+            // No data sent (e.g. Ctrl+C)
+            std::puts("Client disconnected.");
+            std::fflush(stdout);
+            return;
+        }
+        else if (read_size < 0)
+        {
+            std::perror("ERROR: recv() failed");
+            std::exit(EXIT_FAILURE);
+        }
+
+        client_message += std::string(buffer, read_size);
+        // end_pos += client_message.length() - END_MESSAGE.length();
+    }
+
+    std::istringstream input { client_message };
+    std::ostringstream reply;
+
+    static const char *const commands[] = {
+        "READ",
+        "WRITE",
+        "COUNT",
+        "DELETE",
+        "END",
+        NULL
+    };
+
+    std::string command;
+    std::string key;
+    std::string value;
+
+    while (input >> command)
+    {
+        char first_char = command[0];
+        switch (first_char)
+        {
+            case 'R':
+                if (command == "READ") {
+                    input >> key;
+
+                    // Do **NOT** use ```value = KV_DATASTORE[key]```
+                    // for idempotent reads! If the specified key does not exist,
+                    //it inserts it (in this case, with value `""`).
+
+                    // it => std::unordered_map<std::string, std::string>::iterator
+                    //    => decltype(KV_DATASTORE)::iterator
+                    //    => auto
+
+                    value = "NULL"; // if key not found
+                    decltype(KV_DATASTORE)::iterator it = KV_DATASTORE.find(key);
+                    if (it != KV_DATASTORE.end()) {
+                        // Key found in umap
+                        value = it->second;
+                    }
+                    reply << value << '\n';
+                }
+                break; // out of switch
+
+            case 'W':
+                if (command == "WRITE") {
+                    input >> key;
+                    input >> value;
+
+                    if (value[0] != ':' ) {
+                        // start of value indicated by ':'
+                        // format <KEY><newline>:<VALUE><newline>
+                        continue; // while loop
+                    }
+
+                    value.erase(0, 1); // skip the ':'
+                    KV_DATASTORE[key] = value;
+
+                    // Server responds with `FIN` after WRITE
+                    reply << "FIN\n";
+                }
+                break; // out of switch
+
+            case 'C':
+                if (command == "COUNT") {
+                    // Send no. of key-value pairs in `KV_DATASTORE`
+                    reply << KV_DATASTORE.size() << '\n';
+                }
+                break; // out of switch
+
+            case 'D':
+                if (command == "DELETE") {
+                    input >> key;
+                    auto key_found = KV_DATASTORE.erase(key);
+                    // .erase():
+                    //  takes key as argument, returns no. of keys erased
+                    if (key_found) {
+                        value = "FIN\n";
+                    } else {
+                        value = "NULL\n";
+                    }
+                    reply << value;
+                }
+                break; // out of switch
+
+            case 'E':
+                if (command == "END") {
+                    // Final newline for test cases
+                    reply << '\n';
+
+                    // Dispatch complete reply to client
+                    size_t reply_len = reply.str().length();
+                    send(client_sock, reply.str().c_str(), reply_len, 0);
+
+                    // Close the client socket FD.
+                    if (close(client_sock) < 0) {
+                        std::perror("ERROR: close() on client");
+                        std::exit(EXIT_FAILURE);
+                    }
+                }
+
+            default:
+                continue; // while loop
+
+        } // end switch
+    } // end while(input >> command)
+    
+    return;
+}
+
+
 int main(int argc, char *argv[])
 {
     int portno; /* Port to listen on */
-    /* 
+    /*
     * Check command line arguments
     */
     if (argc != 2) {
@@ -33,13 +183,11 @@ int main(int argc, char *argv[])
     // Server port number taken as command line argument
     portno = std::atoi(argv[1]);
 
-    static std::unordered_map<std::string, std::string> KV_DATASTORE;
-
     // 1. Create a socket
     int server_sock = socket(
         AF_INET,     /* domain: IPv4 */
         SOCK_STREAM, /* type: TCP */
-        0            /* protocol: choose automatically 
+        0            /* protocol: choose automatically
                         based on `domain` and `type`   */
     );
 
@@ -88,167 +236,26 @@ int main(int argc, char *argv[])
     std::puts("Server successfully listening!\nWaiting for incoming connections...");
 
 
-
     // 4. Accept connections
     struct sockaddr_in client_addr;
     socklen_t client_socklen = sizeof client_addr;
 
     int client_sock;
 
-accept_connections:
     while ((client_sock = accept(
         server_sock,
         (struct sockaddr *)&client_addr,
         &client_socklen
     )) > 0)
     {
-
-        std::printf("Connection with %d successfully established!\n", client_sock);
-
-
-        // Receive data from client
-        std::string client_message { "" };
-        int read_size;
-        // size_t end_pos = 0;
-
-        const std::string END_MESSAGE = "END\r\n";
-
-        // while (client_message.find("END", end_pos) == std::string::npos)
-        while (client_message.find("END") == std::string::npos)
-        {
-            char buffer[255];
-            read_size = recv(client_sock, &buffer, sizeof(buffer), 0);
-
-            if (read_size == 0)
-            {
-                // No data sent (e.g. Ctrl+C)
-                std::puts("Client disconnected.");
-                std::fflush(stdout);
-                goto accept_connections;
-            }
-            else if (read_size < 0)
-            {
-                std::perror("ERROR: recv() failed");
-                std::exit(EXIT_FAILURE);
-            }
-
-            client_message += std::string(buffer, read_size);
-            // end_pos += client_message.length() - END_MESSAGE.length();
-        }
-
-        std::istringstream input { client_message };
-        std::ostringstream reply;
-
-        static const char *const commands[] = {
-            "READ",
-            "WRITE",
-            "COUNT",
-            "DELETE",
-            "END",
-            NULL
-        };
-
-        std::string command;
-        std::string key;
-        std::string value;
-
-        while (input >> command)
-        {
-            char first_char = command[0];
-            switch (first_char)
-            {
-                case 'R':
-                    if (command == "READ") {
-                        input >> key;
-
-                        // Do **NOT** use ```value = KV_DATASTORE[key]```
-                        // for idempotent reads! If the specified key does not exist,
-                        //it inserts it (in this case, with value `""`).
-
-                        // it => std::unordered_map<std::string, std::string>::iterator
-                        //    => decltype(KV_DATASTORE)::iterator
-                        //    => auto
-
-                        value = "NULL"; // if key not found
-                        decltype(KV_DATASTORE)::iterator it = KV_DATASTORE.find(key);
-                        if (it != KV_DATASTORE.end()) {
-                            // Key found in umap
-                            value = it->second;
-                        }
-                        reply << value << '\n';
-                    }
-                    break; // out of switch
-
-                case 'W':
-                    if (command == "WRITE") {
-                        input >> key;
-                        input >> value;
-
-                        if (value[0] != ':' ) {
-                            // start of value indicated by ':'
-                            // format <KEY><newline>:<VALUE><newline>
-                            continue; // while loop
-                        }
-
-                        value.erase(0, 1); // skip the ':'
-                        KV_DATASTORE[key] = value;
-
-                        // Server responds with `FIN` after WRITE
-                        reply << "FIN\n";
-                    }
-                    break; // out of switch
-
-                case 'C':
-                    if (command == "COUNT") {
-                        // Send no. of key-value pairs in `KV_DATASTORE`
-                        reply << KV_DATASTORE.size() << '\n';
-                    }
-                    break; // out of switch
-
-                case 'D':
-                    if (command == "DELETE") {
-                        input >> key;
-                        auto key_found = KV_DATASTORE.erase(key);
-                        // .erase():
-                        //  takes key as argument, returns no. of keys erased
-                        if (key_found) {
-                            value = "FIN\n";
-                        } else {
-                            value = "NULL\n";
-                        }
-                        reply << value;
-                    }
-                    break; // out of switch
-
-                case 'E':
-                    if (command == "END") {
-                        // Final newline for test cases
-                        reply << '\n';
-
-                        // Dispatch complete reply to client
-                        size_t reply_len = reply.str().length();
-                        send(client_sock, reply.str().c_str(), reply_len, 0);
-
-                        // Close the client socket FD.
-                        if (close(client_sock) < 0) {
-                            std::perror("ERROR: close() on client");
-                            std::exit(EXIT_FAILURE);
-                        }
-                    }
-
-                default:
-                    continue; // while loop
-
-            } // end switch
-        } // end while(input >> command)
-    } // end `while(accept())`
+        handle_connection(client_sock);
+    }
 
     if (client_sock < 0)
     {
         std::perror("ERROR: Server failed to accept connection");
         std::exit(EXIT_FAILURE);
     }
-
 
     return EXIT_SUCCESS;
 }
